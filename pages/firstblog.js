7df2f71b56a609b86c0318086ce79c3d7c0b8add
ChiemekaAnunkor <anunkorjr@gmail.com>
2022-06-11 14:44:05 -0500

    first commit

--- pages/firstblog.js
+++ pages/firstblog.js
@@ -14,7 +14,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to my personal blog page <a href="https://nextjs.org">Next.js!</a>
+          sdsdsdsd<a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
